b481b866d861fd2c5f4dabb387466734e8c85021
amosmaundundu <amosmaundu5@gmail.com>
2022-09-21 11:13:24 +0300

    src

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import user from "../data/user";
+
 
 function Home(props) {
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
+     
+      
+      <h1 style={{ color: user.color }}>
         {props.name} is a Web Developer from {props.city}
       </h1>
     </div>
